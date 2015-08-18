0f067d37f7d337df02d2ade9612ae85185c9b93e
asfly <asfly.zuo@gmail.com>
2015-08-18 21:57:20 +0800

    2015-08-18

---
+++ Web/obj/Release/Package/PackageTmp/Scripts/services/UserService.js
@@ -0,0 +1,18 @@
+﻿/*
+* create by daniel.zuo on 2015.4.19
+*/
+KBLApp.service('UserService', ["$rootScope", "ApiService", function ($rootScope, ApiService) {
+    var service = {
+        signIn: function (loginModel) {
+            var loginModel = JSON.stringify(loginModel);
+            return ApiService.post(ApiService.getApiUrl().signIn, {}, loginModel);
+        },
+        checkCustomerCName: function (cname) {
+            return ApiService.post(ApiService.getApiUrl().checkCustomerCName, {}, { 'cname': cname });
+        },
+        checkUserName: function (username) {
+            return ApiService.post(ApiService.getApiUrl().checkUserName, {}, { 'username': username });
+        }
+    }
+    return service;
+}]);
---
+++ Web/obj/Release/Package/PackageTmp/Templates/users/register/RegisterController.js
@@ -0,0 +1,11 @@
+﻿/*
+* Create by daniel.zuo on 4/19/2015
+*/
+KBLApp.controller("User.RegisterController", ['$rootScope', '$scope', '$state', '$stateParams', 'ApiService', 'CommService', 'UserService',
+    function ($rootScope, $scope, $state, $stateParams, ApiService, CommService, UserService) {
+
+        document.getElementById('role.parentCid').addEventListener('input', function (inputtext) {
+
+        });
+        
+    }]);
