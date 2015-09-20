20d9d212445eb3240cfa22015b51a76252eb62c2
asfly <asfly.zuo@gmail.com>
2015-09-20 15:54:25 +0800

    2015-09-20

---
+++ Web/obj/Release/Package/PackageTmp/Templates/customer/main/MainController.js
@@ -0,0 +1,7 @@
+﻿KBLApp.controller("Customer.MainController", ['$rootScope', '$scope', '$state', '$stateParams', '$location', 'ApiService', 'CommService', 'CustomerService',
+    function ($rootScope, $scope, $state, $stateParams, $location, ApiService, CommService, CustomerService) {        
+        $scope.tab = function (categoryName) {
+            var s = CommService.base64encode(JSON.stringify(Math.random()));
+            $state.go('customer.type', { type:categoryName,s:s });
+        };        
+}]);
